9a8aea6b3dda6f33204c5dacf1cee6269e950f56
camitoarias <camitoaras622@gmail.com>
2022-11-09 18:08:25 -0500

    prueba para front end

--- src/services/autenticacion.service.ts
+++ src/services/autenticacion.service.ts
@@ -1,10 +1,20 @@
-import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+const generador = require('password-generator');
+const cryptoJS = require('crypto-js');
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class AutenticacionService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(/* Add @inject to inject parameters */) { }
+  GenerarClave() {
+    let clave = generador(8, false);
+    return clave;
+  }
+  CifrarClave(clave: string) {
+    let claveCifrada = cryptoJS.MD5(clave).toString();
+    return claveCifrada;
+  }
 
   /*
-   * Add service methods here
-   */
+ * Add service methods here
+ */
 }
